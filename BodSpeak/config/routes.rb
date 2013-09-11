3a11a6a0538fcbed479a9841764c01b9f08408a5
Jisha Muthiyil <jisha.muthiyil@west.cmu.edu>
2013-09-10 22:33:29 -0400

    created sign_in

--- BodSpeak/config/routes.rb
+++ BodSpeak/config/routes.rb
@@ -2,6 +2,11 @@ SampleApp::Application.routes.draw do
   #get "users/new"
   
   resources :users
+  
+  resources :sessions, only: [:new, :create, :destroy]
+  
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
 
   get "static_pages/home"
 
