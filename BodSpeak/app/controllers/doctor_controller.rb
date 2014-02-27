2ca279bfe383fe38d4f4b91408b13d69e8c1bdf8
jishavm <jisha.muthiyil@sv.cmu.edu>
2014-02-27 14:14:25 -0500

    Update doctor_controller.rb

--- BodSpeak/app/controllers/doctor_controller.rb
+++ BodSpeak/app/controllers/doctor_controller.rb
@@ -1,4 +1,5 @@
 class DoctorController < ApplicationController
+#Controller to handle the dispaly of all users under a doctor
 def show
 @user=User.where(:user_type=>'P')
 @msg=Message.find_by_user_id(@user)
