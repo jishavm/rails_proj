c41ec2bb60c4ebcce08a0a1105b4fe091abafb17
jishavm <jisha.muthiyil@sv.cmu.edu>
2014-02-27 14:18:09 -0500

    Update patient_info.rb

--- BodSpeak/app/models/patient_info.rb
+++ BodSpeak/app/models/patient_info.rb
@@ -1,4 +1,6 @@
 class PatientInfo < ActiveRecord::Base
+  #Patient info model. This has to belong to a user(patient user) and also has the option to set 
+  #anonymous='Y' incase the patient wants to post anonymously
   attr_accessible :age, :anonymous, :gender
   belongs_to :user
   accepts_nested_attributes_for :age, :anonymous, :gender
